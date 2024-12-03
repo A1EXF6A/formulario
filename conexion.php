b87b33b7becebaf4f935b67f59d66d905f945407
A1EXF6A <100436091+A1EXF6A@users.noreply.github.com>
2024-12-03 12:24:11 -0500

    se añade los archivos del fornulario

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+$conexion = mysqli_connect('localhost:3308', 'root', '', 'solicitudes');
+
+/*
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}else{
+    echo 'se conecto';
+}*/
+?>
